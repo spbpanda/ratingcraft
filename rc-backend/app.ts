dac72a4eb84c82995d88d8bbff994d73989716da
Alexander Koval <akoval@bivgroup.ru>
2025-03-21 12:15:44 +0300

    app 0.0.0.0

--- rc-backend/app.ts
+++ rc-backend/app.ts
@@ -272,6 +272,6 @@ app.get('/api', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, 'localhost', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
