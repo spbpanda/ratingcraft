06359307e6beae626d952d411174fdfd58d9379e
Alexander Koval <akoval@bivgroup.ru>
2025-03-21 11:17:19 +0300

    change port

--- rc-backend/app.ts
+++ rc-backend/app.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from './middleware/auth'; // Импортируем mi
 import { pingMinecraftServer } from './minecraft-ping/minecraft-ping';
 
 const app = express();
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 10000;
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
