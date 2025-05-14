a28fd76bf7df6d9ba276fde58db954690b4767c5
Alexander Koval <akoval@bivgroup.ru>
2025-05-14 17:40:55 +0300

    backend work prod

--- rc-backend/app.ts
+++ rc-backend/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 10000;
 
 const corsOptions = {
-  origin: 'http://localhost:3030', // замени на адрес фронтенда  'https://ratingcraft.onrender.com'
+  origin: 'https://ratingcraft.onrender.com', // замени на адрес фронтенда  'https://ratingcraft.onrender.com'
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // если используешь cookies
@@ -18,11 +18,11 @@ app.use('/api', routes);
 
 
 // // Start server prod
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server is running on http://0.0.0.0:${PORT}`);
-// });
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
 
 // Start server
-app.listen(PORT, 'localhost', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, 'localhost', () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
